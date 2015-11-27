9c14ba1b9d348c9bf363c0d73d6e3853ece2f984
IChocolateKapa <echochocolatekapa@163.com>
2015-11-27 12:33:20 +0800

    +css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {name: 'Echo', age: 24, gender: "female"});
+  res.render('index', {name: 'Echo', age: 24, gender: "female", cols: [{name: "Shasha", age: 23}, {name: 'shaohua', age: 34}]});
 });
 
 module.exports = router;
