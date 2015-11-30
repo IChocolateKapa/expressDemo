fa5ee5ba901bb8c41e68f908d35bcac5480133c9
IChocolateKapa <echochocolatekapa@163.com>
2015-11-30 10:50:03 +0800

    测试http发起请求与接收get和post请求参数

--- public/js/page_index.js
+++ public/js/page_index.js
@@ -5,4 +5,18 @@
 $(function(){
     var name = data;
     console.log("in index.js file: ", name);
+    $.ajax({
+        url: "/login",
+        type: "post",
+        data: {
+            username: "Echo",
+            password: "123457kshe"
+        },
+        dataType: "json",
+        success: function (data) {
+            console.log("data is : ", data.status);
+            window.open("/quit?username=Echo123&password=123456");//成功！
+        }
+    })
+
 });
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: "这里是第一个页面", name: 'Echo', age: 24, gender: "female", cols: [{name: "Shasha", age: 23}, {name: 'shaohua', age: 34}]});
-  next();
+
+    var price = req.query.price;
+    var vbs = req.query.vbs;//这两种方式是获取get请求的参数
+    console.log("vbs is: ", vbs);
+
+    res.render('index', {title: "这里是第一个页面", name: 'Echo', age: 24, gender: "female", cols: [{name: "Shasha", age: 23}, {name:   'shaohua', age: 34}], price: price});
 });
+
+
+router.post('/login', function(req, res, next) {
+
+    var username = req.body.username;
+    var password = req.body.password;//这两种方式是获取post请求的参数
+    console.log("username is: ", username);
+
+    //res.setHeader({"status": 200});
+    res.send({status: "success"});
+
+});
+router.get('/quit', function(req, res, next) {
+
+    var username = req.query.username;
+    var password = req.query.password;//这两种方式是获取get请求的参数
+    console.log("password is: ", password);
+
+    res.render('login', {username: username, password: password});
+});
+
 
 module.exports = router;
