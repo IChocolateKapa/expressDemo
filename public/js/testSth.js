dffd2aa6a2a0fd9076b17a883c219cdc1e042292
IChocolateKapa <echochocolatekapa@163.com>
2015-12-28 18:09:19 +0800

    函数的学习

--- public/js/testSth.js
+++ public/js/testSth.js
@@ -55,11 +55,47 @@ function subFunc () {
 }
 
 subObj = new subFunc();
-console.log(subObj);
+//console.log(subObj);
 
 
-subObj.prototype.getName = function () {
+/*subObj.prototype.getName = function () {
     return this.name;
 
+}*/
+//subObj.getName();
+
+
+/*正则表达式 -测试-*/
+var password = "234643er";
+if (/\d/.test(password)
+    && /[A-Z]/.test(password)
+    && /[a-z]/.test(password) ) {
+
+    //变绿
+    console.log("true");
+} else {
+    console.log("false");
 }
-subObj.getName();
+/*正则表达式 -测试-*/
+
+
+
+
+
+function a () {
+    console.log(3);
+}
+if (3 < 2) {
+    function a() {
+        console.log(4);
+    }
+    console.log(6);
+}
+a();//执行结果， 不可思议的 是 ： 4
+// anyone can explain?
+//ok, no one, then, i will
+// 从本文件一开始， 就说明了， js对于函数内部的 变量和函数的提升
+//在script标签内包着的代码， 执行环境是 全局环境
+// 所以， js引擎会把两个a 函数提升至全局环境初始， 由于if 语句处于第一个a之后， 所以， 变量提升后， 后面的声明会覆盖掉前面的
+//在引擎眼中， a的定义时 打印 4 这个方法。
+//此坑【不算坑， 只能说没吃透js的机制】，初次发现是在 银翼项目中， 当某个开关开启时， Require一个其它模块；线上是关闭这个开关，并且没有上传这个模块文件的。理论上感觉应该没问题。结果报错 ，找不到那个未上传的模块。于是乎， 少华大大让俺更深记住了这个问题
